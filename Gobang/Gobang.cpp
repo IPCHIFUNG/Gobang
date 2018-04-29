5245ffe8fe70c38cfd6425f480028d03049a4d4b
ZiFung <791122475@qq.com>
2018-04-29 21:57:04 +0800

    棋盘保存与读取

--- Gobang/Gobang.cpp
+++ Gobang/Gobang.cpp
@@ -1,5 +1,7 @@
 #include "Gobang.h"
 
+#include <stdio.h>
+
 Gobang::Gobang()
 {
 	// 初始化棋盘
@@ -23,6 +25,57 @@ void Gobang::initBoard()
 		for (int j = 0; j < BOARDLENGTH; j++)
 			board[i][j] = ChessType::NOCHESS;
 	turn = ChessType::BLACKCHESS;
+}
+
+/*
+	把棋盘保存到文件里
+
+	@author 叶志枫
+	@para 保存路径
+	@throw char *
+*/
+void Gobang::saveBoard(char * path)
+{
+	FILE * outFile;
+
+	outFile = fopen(path, "w");
+	if (outFile == nullptr)
+		throw "Unable to open file.";
+
+	int size = steps->size();
+	fprintf(outFile, "%d\n", size);
+	auto iterator = steps->begin();
+	for (int i = 0; i < size; i++)
+	{
+		Step tmp = *iterator;
+		fprintf(outFile, "%d\t%d\n", tmp.x, tmp.y);
+		iterator++;
+	}
+	fclose(outFile);
+}
+
+/*
+	从文件里读取棋盘
+
+	@author 叶志枫
+	@para 保存路径
+*/
+void Gobang::loadBoard(char * path)
+{
+	FILE * inFile;
+
+	inFile = fopen(path, "r");
+	if (inFile == nullptr)
+		throw "Unable to open file.";
+	int size = 0;
+	fscanf(inFile, "%d\n", &size);
+	Step tmp;
+	for (int i = 0; i < size; i++)
+	{
+		fscanf(inFile, "%d\t%d\n", &tmp.x, &tmp.y);
+		steps->push_back(tmp);
+	}
+	turn = size % 2;
 }
 
 /*
