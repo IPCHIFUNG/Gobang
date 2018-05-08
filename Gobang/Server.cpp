e4c929a1ea19cabcd9e2133f2040ad3398c01255
ZiFung <791122475@qq.com>
2018-05-08 19:12:00 +0800

    update server class

---
+++ Gobang/Server.cpp
@@ -0,0 +1,29 @@
+#include "Server.h"
+
+Server::Server(char * IPAddr, int port)
+{
+}
+
+Server::Server(int port)
+{
+}
+
+Server::~Server()
+{
+}
+
+void Server::openListeningThread()
+{
+}
+
+void Server::openConnectingThread()
+{
+}
+
+void Server::listeningThread()
+{
+}
+
+void Server::connectingThread()
+{
+}
