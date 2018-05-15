27c6da20bc0131909c58aabe38807a8066d0f53a
alexandf <18801023305@163.com>
2018-05-15 20:01:47 +0800

    internet1

--- Gobang/Server.h
+++ Gobang/Server.h
@@ -2,20 +2,32 @@
 #define SERVER_H
 
 #include <thread>
+#include<stdio.h>
+#include <stdlib.h>
+#include<WinSock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#include <iostream>
+#include<string>
+
+using namespace std;
 
 class Server
 {
 public:
-	Server(char * IPAddr, int port);
+	Server(sockaddr_in addr, int port);
 	Server(int port);
 	~Server();
 
 private:
-
+	void server_begin(char*);
+	void client_begin(char*);
 private:
 	int port;
-	char * IPAddr;
-
+	sockaddr_in addr;
+	//char * IPAddr;
+	WSADATA data;
 };
 
 
