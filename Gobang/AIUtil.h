df02e58d0bd990adb6dfd5faa144a515370e26e4
yyy644751509 <644751509@qq.com>
2018-05-22 16:19:54 +0800

    AIUtil

---
+++ Gobang/AIUtil.h
@@ -0,0 +1,12 @@
+#ifndef AIUTIL_H
+#define AIUTIL_H
+
+class AIUtil
+{
+public:
+
+private:
+
+};
+#endif // !AIUTIL_H
+
--- Gobang/Gobang.cpp
+++ Gobang/Gobang.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "Gobang.h"
+#include "AIUtil.h"
 
 Gobang::Gobang()
 {
